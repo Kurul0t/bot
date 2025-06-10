b07f60ce290afbb967cac1786d53955348e329a6
Kurulot <kiril21pr17@gmail.com>
2025-06-10 21:28:08 +0300

    версія 1.1.4

--- botbot.py
+++ botbot.py
@@ -362,7 +362,7 @@ async def check_periodically(bot: Bot):
                     print("❌ Дата не збігається.")
             else:
                 print("Час перевірки! Але дати немає.")
-        elif now.hour == 21 and now.minute == 25:
+        elif now.hour == 21 and now.minute == 34:
             logger.info("час співпадає")
             if "date" in state_day_start:
                 logger.info("вибір дня")
